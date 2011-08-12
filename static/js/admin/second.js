f6dbaa5324e13124825f8ddd1ffa3c43d9dbcc42
cage <517020375@qq.com>
2011-08-12 12:21:33 +0800

    第三版后台

--- static/js/admin/second.js
+++ static/js/admin/second.js
@@ -1,4 +1,12 @@
-$(document).ready(
-function(){
+$(document).ready(function(){
+	var $body = $("body");
+	var $header = $("#header");
+	var $curposit = $("#cur_posit");
+	var $frame = $("#frame");
 
+	$frame.css("height",$body.height() - 89 + "px");
+
+	$(window).resize(function(){
+		$frame.css("height",$body.height() - 89 + "px");
+	});
 });
