36501c52beb0357079b3e26e6fb6a111d69bc96f
包魁 <baokui@cctvcjw.com>
2011-09-01 10:56:18 +0800

    shark静态环境搭建

---
+++ templates/demo.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html class="demo">
+<head>
+<title>后台</title>
+<meta charset="utf-8" />
+<link type="text/css" rel="stylesheet" href="../static/css/admin/admin.css" />
+<script type="text/javascript" src="../static/js/public/jquery.min.js"></script>
+<script type="text/javascript" src="../static/js/admin/shark-ui.js"></script>
+<script type="text/javascript" src="../static/js/public/jplugins/easing.js"></script>
+<script type="text/javascript" src="../static/js/public/jplugins/hotkeys.js"></script>
+<script type="text/javascript" src="../static/js/admin/base.js"></script>
+</head>
+
+<body>
+	<div class="main_menu">
+		<div class="user">
+			您好，Administrators!
+			<i class="arrow"></i>
+			<ul>
+				<li><a href="#">修改密码</a></li>
+				<li><a href="#">个人信息</a></li>
+				<li><a href="#">帐号管理</a></li>
+				<li><a href="#">删除帐号</a></li>
+				<li><a href="#">更换帐号</a></li>
+				<li><a href="#">注销登录</a></li>
+			</ul>
+		</div>
+		<dl id="left_nav">
+			<dt class="show">
+				内容发布管理
+				<i class="icon">&nbsp;</i>
+			</dt>
+			<dd class="show">
+				<ul>
+					<li><a href="login.html">login.html</a></li>
+					<li class="current"><a href="pop.html">pop.html</a></li>
+					<li><a href="default.html">default.html</a></li>
+					<li><a href="index.html">index.html</a></li>
+					<li><a href="jqui-draggable.html">jqui-draggble.html</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+				</ul>
+			</dd>
+			<dt>
+				发布管理
+				<i class="icon">&nbsp;</i>
+			</dt>
+			<dd>
+				<ul>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+				</ul>
+			</dd>
+			<dt>
+				内容相关设置
+				<i class="icon">&nbsp;</i>
+			</dt>
+			<dd>
+				<ul>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+				</ul>
+			</dd>
+			<dt>
+				私募管理
+				<i class="icon">&nbsp;</i>
+			</dt>
+			<dd>
+				<ul>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+					<li><a href="#">菜单栏目名称</a></li>
+				</ul>
+			</dd>
+		</dl>
+		<div class="menu_onff">
+			<span>
+				.
+				<br/>
+				.
+				</br>
+				.
+			</span>
+		</div>
+        <div class="skin_box">
+        	<a class="switch_skin" href="javascript:void('默认')" title="默认">默认</a>
+        	<a class="switch_skin" href="javascript:void('风格1')" title="风格1">风格1</a>
+            <a class="switch_skin" href="javascript:void('风格2')" title="风格2">风格2</a>
+        	<a class="switch_skin" href="javascript:void('风格3')" title="风格3">风格3</a>
+            <a class="switch_skin" href="javascript:void('风格4')" title="风格4">风格4</a>
+        </div>
+	</div>
+	<div class="workspace">
+		<div class="panle panle_top">
+			<div class="cur_position">
+				<span>当前位置：</span>
+				<a href="#">首页</a>
+				&gt;
+				<a href="#">视频</a>
+				&gt;
+				<a href="#">详情</a>
+			</div>		
+		</div>
+		<div class="panle panle_bottom">
+			<span class="skin_btn" title="更换任务栏背景">&nbsp;</span>
+			<ul class="taskbar">
+			</ul>
+			<div class="switch_workspace">
+				<span title="工作区一 Alt+1" class="current">&nbsp;</span>
+				<span title="工作区二 Alt+2">&nbsp;</span>
+				<span title="工作区三 Alt+3">&nbsp;</span>
+				<span title="工作区四 Alt+4">&nbsp;</span>
+			</div>
+		</div>
+		<div class="main_cont">
+		</div>
+	</div>
+	<div class="workspace">
+		<div class="panle panle_top">
+			<div class="cur_position">
+				<span>当前位置：</span>
+				<a href="#">首页</a>
+				&gt;
+				<a href="#">视频</a>
+				&gt;
+				<a href="#">详情</a>
+			</div>		
+		</div>
+		<div class="panle panle_bottom">
+			<span class="skin_btn" title="更换任务栏背景">&nbsp;</span>
+			<ul class="taskbar">
+			</ul>
+			<div class="switch_workspace">
+				<span title="工作区一 Alt+1">&nbsp;</span>
+				<span title="工作区二 Alt+2" class="current">&nbsp;</span>
+				<span title="工作区三 Alt+3">&nbsp;</span>
+				<span title="工作区四 Alt+4">&nbsp;</span>
+			</div>
+		</div>
+		<div class="main_cont">
+		</div>
+	</div>
+	<div class="workspace">
+		<div class="panle panle_top">
+			<div class="cur_position">
+				<span>当前位置：</span>
+				<a href="#">首页</a>
+				&gt;
+				<a href="#">视频</a>
+				&gt;
+				<a href="#">详情</a>
+			</div>		
+		</div>
+		<div class="panle panle_bottom">
+			<span class="skin_btn" title="更换任务栏背景">&nbsp;</span>
+			<ul class="taskbar">
+			</ul>
+			<div class="switch_workspace">
+				<span title="工作区一 Alt+1">&nbsp;</span>
+				<span title="工作区二 Alt+2">&nbsp;</span>
+				<span title="工作区三 Alt+3" class="current">&nbsp;</span>
+				<span title="工作区四 Alt+4">&nbsp;</span>
+			</div>
+		</div>
+		<div class="main_cont">
+		</div>
+	</div>
+	<div class="workspace">
+		<div class="panle panle_top">
+			<div class="cur_position">
+				<span>当前位置：</span>
+				<a href="#">首页</a>
+				&gt;
+				<a href="#">视频</a>
+				&gt;
+				<a href="#">详情</a>
+			</div>		
+		</div>
+		<div class="panle panle_bottom">
+			<span class="skin_btn" title="更换任务栏背景">&nbsp;</span>
+			<ul class="taskbar">
+			</ul>
+			<div class="switch_workspace">
+				<span title="工作区一 Alt+1">&nbsp;</span>
+				<span title="工作区二 Alt+2">&nbsp;</span>
+				<span title="工作区三 Alt+3">&nbsp;</span>
+				<span title="工作区四 Alt+4" class="current">&nbsp;</span>
+			</div>
+		</div>
+		<div class="main_cont">
+		</div>
+	</div>
+</body>
+</html>
